10d06991669d6560b9abc3b0f57235cdf28616c3
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-20 17:06:48 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,43 @@
 <template>
-    <header>
-        ciaooooooooooooooooo,
+    <header class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+        </div>
+
+        <nav>
+            <ul class="menu">
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                                <li>
+                    <a href="#">TV</a>
+                </li>
+                                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>                
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -9,3 +46,27 @@ export default {
     name: "Header"
 }
 </script>
+
+<style scoped lang="scss">
+header {
+    height: 150px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: aqua;
+    .logo {
+        img{
+            width: 85px;
+        }
+    }
+    .menu {
+        display: flex;
+        list-style-type: none;
+        justify-content: center;
+        li{
+            padding: 0 10px;
+            font-size: 13px;
+        }
+    }
+}
+</style>
