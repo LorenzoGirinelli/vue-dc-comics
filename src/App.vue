0312599de556e9ca0726a0cb7c8a493381f7d46b
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-21 17:44:17 +0100

    header dinamico

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header />,
-    <Main />
+    <Main />,
+    <Icons />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
+import Icons from "./components/Icons.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Main,
+    Icons,
   },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,35 +6,8 @@
 
         <nav>
             <ul class="menu">
-                <li>
-                    <a href="#">CHARACTERS</a>
-                </li>
-                                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                                <li>
-                    <a href="#">MOVIES</a>
-                </li>
-                                <li>
-                    <a href="#">TV</a>
-                </li>
-                                <li>
-                    <a href="#">GAMES</a>
-                </li>
-                                <li>
-                    <a href="#">COLLECTIBLES</a>
-                </li>                
-                <li>
-                    <a href="#">VIDEOS</a>
-                </li>
-                <li>
-                    <a href="#">FANS</a>
-                </li>
-                <li>
-                    <a href="#">NEWS</a>
-                </li>
-                <li>
-                    <a href="#">SHOP</a>
+                <li v-for="(link, index) in links" :key"index" :class="{ current: link.current }">
+                    <a :href="link.url">{{ link.text }}</a>
                 </li>
             </ul>
         </nav>
@@ -43,11 +16,38 @@
 
 <script>
 export default {
-    name: "Header"
+    name: "Header",
+    data: function () {
+        return {
+            links: [
+                {
+                    text: 'Prodotti',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Prodotti',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Prodotti',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Prodotti',
+                    url: '#',
+                    current: false
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
+@import '../style/variables';
 header {
     height: 150px;
     display: flex;
