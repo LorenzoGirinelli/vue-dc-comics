0c906c2216d5d6ffd35c4ce8e1f4d9f27a8ca284
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-21 18:03:24 +0100

    header dinamico finito

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,82 +1,161 @@
 <template>
-    <header class="container">
-        <div class="logo">
-            <img src="../assets/img/dc-logo.png" alt="logo">
-        </div>
+    <header>
+        <div class="container">
 
-        <nav>
-            <ul class="menu">
-                <li v-for="(link, index) in links" :key"index" :class="{ current: link.current }">
-                    <a :href="link.url">{{ link.text }}</a>
-                </li>
-            </ul>
-        </nav>
+                <!-- logo  -->
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="Logo dc">
+                </div>
+
+                <!-- Menu  -->
+                <div class="header-menu">
+                    <ul>
+                        <li 
+                            v-for="(link, index) in links" 
+                            :key="index"
+                            :class="{ active: link.active }"
+                        >
+                            <a :href="link.href">{{ link.text }}</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
     </header>
 </template>
 
 <script>
 export default {
     name: "Header",
-    data: function () {
+    data: function() {
         return {
             links: [
                 {
-                    text: 'Prodotti',
-                    url: '#',
-                    current: false
+                    href: '#',
+                    text: 'characters',
+                    active: false,
                 },
                 {
-                    text: 'Prodotti',
-                    url: '#',
-                    current: true
+                    href: '#',
+                    text: 'comics',
+                    active: true,
                 },
                 {
-                    text: 'Prodotti',
-                    url: '#',
-                    current: false
+                    href: '#',
+                    text: 'movies',
+                    active: false,
                 },
                 {
-                    text: 'Prodotti',
-                    url: '#',
-                    current: false
+                    href: '#',
+                    text: 'tv',
+                    active: false,
                 },
-            ]
+                {
+                    href: '#',
+                    text: 'games',
+                    active: false,
+                },
+                {
+                    href: '#',
+                    text: 'collectibles',
+                    active: false,
+                },
+                {
+                    href: '#',
+                    text: 'videos',
+                    active: false,
+                },
+                {
+                    href: '#',
+                    text: 'fans',
+                    active: false,
+                },
+                {
+                    href: '#',
+                    text: 'news',
+                    active: false,
+                },
+                {
+                    href: '#',
+                    text: 'shop',
+                    active: false,
+                },
+            ],
         }
-    }
+    },
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import '../style/variables';
 header {
-    height: 150px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: aqua;
-    .logo {
-        img{
-            width: 85px;
+    width: 100%;
+    height: 120px;
+    .container {
+            padding: 0 20px;
+            display: flex;
+            justify-content: space-between;
+            .logo {
+                max-width: 90px;
+                vertical-align: middle;
+                img {
+                    margin-top: 10px;
+                }
+            }
+            .header-menu {
+                li {
+                    display: inline-block;
+                    line-height: 110px;
+                    margin: 0 12px;
+                    font-size: 14px;
+                    text-transform: uppercase;
+                    font-weight: bold;
+                    color: #535759;
+                    &.active {
+                        border-bottom: 5px solid $brand_primary_color;
+                        color: $brand_primary_color;
+                    }
+                }
+                a {
+                    text-decoration: none;
+                    font-weight: bold;
+                    color: #535759;
+                }
+            }
         }
-    }
-    .menu {
-        display: flex;
-        list-style-type: none;
-        justify-content: center;
-        li{
-            padding: 0 10px;
-            font-size: 13px;
-        }
-        a {
-            text-decoration: none;
-            font-weight: bold;
-            color: #535759;
-        }
-        a:hover{
-            color: #0282f9;
-            padding-bottom: 63px;
-            border-bottom: 5px solid #0282f9;
-        }
-    }
 }
-</style>
+</style> 
+
+// <style scoped lang="scss">
+// header {
+//     height: 150px;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     background-color: aqua;
+//     .logo {
+//         img{
+//             width: 85px;
+//         }
+//     }
+//     .menu {
+//         display: flex;
+//         list-style-type: none;
+//         justify-content: center;
+//         li{
+//             padding: 0 10px;
+//             font-size: 13px;
+//         }
+        // a {
+        //     text-decoration: none;
+        //     font-weight: bold;
+        //     color: #535759;
+        // }
+//         a:hover{
+//             color: #0282f9;
+//             padding-bottom: 63px;
+//             border-bottom: 5px solid #0282f9;
+//         }
+//     }
+// }
+// </style>
